97dbfd4b305c18aaed87bf0fdd5c51f91b8283e2
Aefvvges <torri.barbara4@gmail.com>
2024-04-19 21:55:19 -0300

    com

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$nombre = $_GET['nombre'];
+$apellido = $_GET['apellido'];
+$edad = $_GET['edad'];
+$ciudad = $_GET['ciudad'];
+$nacionalidad = $_GET['nacionalidad'];
+
+echo 'Nombre: ' . $nombre;
+echo '<br>';
+echo 'Apellido: ' . $apellido;
+echo '<br>';
+echo 'Edad: ' . $edad;
+echo '<br>';
+echo 'Ciudad: ' . $ciudad;
+echo '<br>';
+echo 'Nacionalidad: ' . $nacionalidad;
